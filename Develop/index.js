6b9be01a8ae7e771fb67295cb6030e68ab07b3c4
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-02-17 22:06:08 -0500

    started working on my homework

---
+++ Develop/index.js
@@ -0,0 +1,29 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
+
+const generatorMarkdown = require('./utils/generateMarkdown');
+
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type = "input",
+    message: "What is your Github username?"
+
+}];
+//What is yor email address?
+//what is your project's name?
+//short description?
+//licence?
+//install dependencies
+//run to ru n test
+//using repo
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
